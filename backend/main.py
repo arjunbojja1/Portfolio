1199e49723384c1fad280fc240b03eeba9d00ea7
Arjun Bojja <arjunbojja1@gmail.com>
2025-07-08 19:21:30 -0400

    Added middleware configuration for backend

--- backend/main.py
+++ backend/main.py
@@ -12,3 +12,11 @@ origins = [
     "http://localhost:3000",
     "localhost:3000",
 ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
