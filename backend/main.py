f7720a31cb1c2af5651ddd1fe4106e0c8de78e61
Arjun Bojja <arjunbojja1@gmail.com>
2025-07-08 19:20:21 -0400

    Set up FastAPI app and CORS middleware

--- backend/main.py
+++ backend/main.py
@@ -3,3 +3,12 @@ import os
 from fastapi import FastAPI, HttpException, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, EmailStr
+
+# FastAPI app
+app = FastAPI()
+
+# CORS Middleware Configuration
+origins = [
+    "http://localhost:3000",
+    "localhost:3000",
+]
